c293d52b9ba9177cd4076ad47de2436afda959b3
Breakfast34 <82320414+Breakfast34@users.noreply.github.com>
2021-11-02 22:25:58 +0700

    Update Formtable.php

--- views/Formtable.php
+++ views/Formtable.php
@@ -5,8 +5,33 @@
         <td><input class="form-control"></td>
         <td><input class="form-control"></td>
     </tr>
+
     <tr>
-        <th scope="row">2. ค่าธรรมเนียม ค่านายหน้า</th>
+        <th scope="row">2. ค่าธรรมเนียม ค่านายหน้า ฯลฯ ตามมาตรา 40 (2)</th>
+        <td><input class="form-control"></td>
+        <td><input class="form-control"></td>
+        <td><input class="form-control"></td>
+    </tr>
+
+    <tr>
+        <th scope="row">3. ค่าแห่งลิขสิทธิ์ ฯลฯ ตามมาตรา 40 (3)</th>
+        <td><input class="form-control"></td>
+        <td><input class="form-control"></td>
+        <td><input class="form-control"></td>
+    </tr>
+
+    <tr>
+        <th scope="row" class="">4. (ก) ดอกเบี้ย ฯลฯ ตามมาตรา 40 (4) (ก)
+            <br> &nbsp;&nbsp;&nbsp; (ข) เงินปันผล เงินส่วนแบ่งกำไร ฯลฯ ตามมาตรา 40 (4) (ข)
+            <br> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; กำไรสุทธิของกิจการที่ต้องเสียภาษีเงินได้นิติบุคคลในอัตราดังนี้
+        </th>
+        <td> <input class="form-control"></td>
+        <td><input class="form-control"></td>
+        <td><input class="form-control"></td>
+    </tr>
+
+    <tr>
+        <th scope="row">(1.1) อัตรา ฯลฯ ตามมาตรา 40 (3)</th>
         <td><input class="form-control"></td>
         <td><input class="form-control"></td>
         <td><input class="form-control"></td>
