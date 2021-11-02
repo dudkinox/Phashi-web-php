075e796d516431cb4bbe8a127d9fce4bcd4e2c20
Breakfast34 <82320414+Breakfast34@users.noreply.github.com>
2021-11-02 22:07:16 +0700

    up date Create Form 1/2

--- views/Form.php
+++ views/Form.php
@@ -1,16 +1,161 @@
-<form>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+<div class="container border border-dark rounded-3 py-3">
+  <form>
+    <div class="form-group">
+      <h5 class="text-center ">หนังสือรับรองการหักภาษี ณ ที่จ่าย</h5>
+      <p class="text-center">ตามมาตรา 50 ทวิ แห่งประมวลรัษฎากร</p>
+    </div>
+  </form>
+
+  <div class="container justify-content-between border border-dark rounded-3 py-3">
+    <form>
+      <div class=" row">
+        <div class="col-md-5">
+          <label class="">ผู้มีหน้าที่หักภาษี ณ ที่จ่าย : -</label>
+        </div>
+        <div class="col-md-3">
+          <label class="col-form-label">เลขประจำตัวผู้เสียภาษีอากร (13 หลัก)*</label>
+        </div>
+        <div class="col-md-4">
+          <input class="form-control">
+        </div>
+      </div>
+      <div class="row pt-3">
+        <div class="col-md-6 ">
+          <label class="form-label">ชื่อ</label>
+          <input class="form-control" placeholder="(ให้ระบุว่าเป็น บุคคล นิติบุคคล บริษัท สมาคม หรือคณะบุคคล)">
+        </div>
+        <div class="col-md-6">
+          <label class="form-label">เลขประจำตัวผู้เสียภาษีอากร</label>
+          <input class="form-control">
+        </div>
+      </div>
+      <div class="mb-3 row pt-3">
+        <label for="inputPassword" class="col-md-1 col-form-label">ที่อยู่</label>
+        <div class="col-md-11">
+          <input class="form-control" placeholder="(ให้ระบุชื่ออาคาร/หมู่บ้าน ห้องเลขที่ชั้นที่ เลขที่ ตรอก/ซอย หมู่ที่ถนน ตำบล/แขวง อำเภอ/เขต จังหวัด)">
+        </div>
+      </div>
+    </form>
   </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  <div class="container border border-dark rounded-3 py-3">
+    <form>
+      <div class=" row">
+        <div class="col-md-5">
+          <label class="">ผู้มีหน้าที่หักภาษี ณ ที่จ่าย : -</label>
+        </div>
+        <div class="col-md-3">
+          <label class="col-form-label">เลขประจำตัวผู้เสียภาษีอากร (13 หลัก)*</label>
+        </div>
+        <div class="col-md-4">
+          <input class="form-control">
+        </div>
+      </div>
+      <div class="row pt-3">
+        <div class="col-md-6 ">
+          <label class="form-label">ชื่อ</label>
+          <input class="form-control" placeholder="(ให้ระบุว่าเป็น บุคคล นิติบุคคล บริษัท สมาคม หรือคณะบุคคล)">
+        </div>
+        <div class="col-md-6">
+          <label class="form-label">เลขประจำตัวผู้เสียภาษีอากร</label>
+          <input class="form-control">
+        </div>
+      </div>
+      <div class="row pt-3">
+        <label for="inputPassword" class="col-md-1 col-form-label">ที่อยู่</label>
+        <div class="col-md-11">
+          <input class="form-control" placeholder="(ให้ระบุชื่ออาคาร/หมู่บ้าน ห้องเลขที่ชั้นที่ เลขที่ ตรอก/ซอย หมู่ที่ถนน ตำบล/แขวง อำเภอ/เขต จังหวัด)">
+        </div>
+      </div>
+      <div class="row pt-3">
+        <label for="inputPassword" class="col-md-1 col-form-label">ลำดับที่อยู่</label>
+        <div class="col-md-2">
+          <input class="form-control">
+        </div>
+        <label for="inputPassword" class="col-md-1 col-form-label">ในแบบ</label>
+        <div class="col-md-2">
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+            <label class="form-check-label" for="flexCheckChecked">
+              (1) ภ.ง.ด.1ก
+            </label>
+          </div>
+        </div>
+        <div class="col-md-2">
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+            <label class="form-check-label" for="flexCheckChecked">
+              (2) ภ.ง.ด.1ก พเศษ
+            </label>
+          </div>
+        </div>
+        <div class="col-md-2">
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+            <label class="form-check-label" for="flexCheckChecked">
+              (3) ภ.ง.ด.2
+            </label>
+          </div>
+        </div>
+        <div class="col-md-2">
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+            <label class="form-check-label" for="flexCheckChecked">
+              (4) ภ.ง.ด.3
+            </label>
+          </div>
+        </div>
+        <div class="col-md-4">
+        </div>
+        <div class="col-md-2">
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+            <label class="form-check-label" for="flexCheckChecked">
+              (5) ภ.ง.ด.2ก
+            </label>
+          </div>
+        </div>
+
+        <div class="col-md-2">
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+            <label class="form-check-label" for="flexCheckChecked">
+              (6) ภ.ง.ด.3ก
+            </label>
+          </div>
+        </div>
+        <div class="col-md-2">
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+            <label class="form-check-label" for="flexCheckChecked">
+              (7) ภ.ง.ด.53
+            </label>
+          </div>
+        </div>
+      </div>
+    </form>
   </div>
-  <div class="form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+
+  <table class="table table-bordered border-back">
+    <thead>
+      <tr>
+        <div class="row">
+          <th class="text-center col-md-6">
+            ประเภทเงินได้พึงประเมินที่จ่าย
+          </th>
+          <th class="text-center col-md-2">
+            วัน/เดือน/ปี (ภาษีที่จ่าย)
+          </th>
+          <th class="text-center col-md-2">
+            จำนวนเงินที่จ่าย
+          </th>
+          <th class="text-center col-md-2">
+            ภาษีที่หักและนำส่งไว้
+          </th>
+        </div>
+      </tr>
+    </thead>
+    <?php include 'views/Formtable.php'; ?>
+
+  </table>
+
+</div>
---
+++ views/Formtable.php
@@ -0,0 +1,20 @@
+<tbody>
+    <tr>
+        <th scope="row">1. เงินเดือน ค่าจ้าง โบนัส ฯลฯ ตามมาตรา 40 (1)</th>
+        <td><input class="form-control"></td>
+        <td><input class="form-control"></td>
+        <td><input class="form-control"></td>
+    </tr>
+    <tr>
+        <th scope="row">2. ค่าธรรมเนียม ค่านายหน้า</th>
+        <td><input class="form-control"></td>
+        <td><input class="form-control"></td>
+        <td><input class="form-control"></td>
+    </tr>
+
+    <!-- <tr>
+        <th scope="row">3</th>
+        <td colspan="2">dd</td>
+        <td><input class="form-control"></td>
+      </tr> -->
+</tbody>
